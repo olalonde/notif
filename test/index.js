dd20cccaeedca428c857e420fb1e9b31f777896f
Joshua Rountree <joshua@joshua-x.(none)>
2012-11-27 22:56:43 -0500

    added a 'target' option

--- test/index.js
+++ test/index.js
@@ -1,11 +1,22 @@
 var notif = require('../');
 
 var oli = {
+    objectType:'user',
     username: 'olalonde'
   },
   bobby = {
+    objectType: 'user',
     username: 'bobby'
-  };
+  },
+  project = {
+    objectType:'project',
+    myName:'My Project'
+  }
+  ,
+  picture = {
+    objectType:'photo',
+    name:'Nuts and Bolts'
+  }
 
 notif.register(function (n, cb) {
   console.log(n.actor.username + ' ' + n.verb + ' ' + ((n.object && n.object.username) ? n.object.username : ''));
@@ -17,6 +28,10 @@ notif.register({ only: ['registered', 'deleted account'] }, function (n, cb) {
   console.log('Send an email because ' + n.actor.username + ' ' + n.verb);
 });
 
+notif.register({only: ['uploaded']}, function (n, cb) {
+  console.log(n.actor.username + ' ' + n.verb + ' a ' + n.object.objectType + ' to ' + n.target.myName);
+});
+
 notif.register({ except: ['deleted account'] }, function (n, cb) {
   console.log(n.verb + ' === good news!');
 });
@@ -24,3 +39,4 @@ notif.register({ except: ['deleted account'] }, function (n, cb) {
 notif.send(bobby, 'registered');
 notif.send(bobby, 'followed', oli);
 notif.send(bobby, 'deleted account');
+notif.send(bobby, 'uploaded', picture, project);
